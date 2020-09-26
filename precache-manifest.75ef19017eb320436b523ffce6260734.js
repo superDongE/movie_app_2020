22afe4d53a875939cadc69f67f4fa1ae17d1add9
superDongE <dlagoehd13@gmail.com>
2020-09-26 23:50:30 +0900

    Updates

---
+++ precache-manifest.75ef19017eb320436b523ffce6260734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1d4f00733925103b93e418d1eab14e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "af571699e466e75eb899",
+    "url": "/movie_app_2020/static/css/main.cf0d22cf.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af571699e466e75eb899",
+    "url": "/movie_app_2020/static/js/main.dd093339.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
